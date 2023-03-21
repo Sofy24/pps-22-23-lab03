2940f0884d8fdc58c4bb49f3c75f9358605a534f
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-03-21 16:56:30 +0100

    +Max

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -1,10 +1,10 @@
 package u03
 
 import scala.annotation.tailrec
+import u02.Optionals.*
 
 
 object Lists extends App :
-
   // A generic linkedlist
   enum List[E]:
     case Cons(head: E, tail: List[E])
@@ -18,8 +18,8 @@ object Lists extends App :
 
     @tailrec
     def get[E](list: List[E], index: Int): Option[E] = (list, index) match
-      case (Nil(), _) => None
-      case (Cons(h, t), 0) => Some(h)
+      case (Nil(), _) => Option.None()
+      case (Cons(h, t), 0) => Option.Some(h)
       case (Cons(_, t), i) => get(t, i - 1)
 
     def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
@@ -44,10 +44,29 @@ object Lists extends App :
       case Nil() => Nil()
       case Cons(h, t) => append(f(h), flatMap(t)(f))
 
+    /*def mapSecondVersion[A, B](l: List[A])(mapper: A => B): List[B] =
+      flatMap(l)(mapper)*/
+
+    /*def filterSecondVersion[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
+      case Cons(h, t) => flatMap(t)(pred(h))*/
+
+
+
+    def max(l: List[Int]): Option[Int] = l match
+      case Cons(h, t) => t match
+        case Cons(h3, Nil()) if h > h3 => Option.Some(h)
+        case Cons(h3, Nil()) if h <= h3 => Option.Some(h3)
+        case Cons(h2, t2) if h > h2 => max(Cons(h, t2))
+        case Cons(h2, t2) if h <= h2 => max(t)
+      case Nil() => Option.None()
+
+
+
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
 
   import List.*
+  import Option.*
 
 
   println(sum(map(filter(l)(_ >= 20))(_ + 1))) // 21+31 = 52
